8987e6c659034907b047421d9263755397033c02
IcarusVPS <matthewdegnan93@gmail.com>
2018-05-15 00:57:47 +0100

    fletching

--- src/ethos/model/players/skills/fletching/FletchableBolt.java
+++ src/ethos/model/players/skills/fletching/FletchableBolt.java
@@ -1,13 +1,13 @@
 package ethos.model.players.skills.fletching;
 
 public enum FletchableBolt {
-	SAPPHIRE(9142, 9189, 9337, 54, 5), 
-	EMERALD(9142, 9190, 9338, 61, 7), 
-	RUBY(9143, 9191, 9339, 63, 8), 
-	DIAMOND(9143, 9192, 9340, 65, 9), 
-	DRAGON(9144, 9193, 9341, 71, 10), 
-	ONYX(9144, 9194, 9342, 73, 12),
-	AMETHYST(11875, 21338, 21316, 76, 11);
+	SAPPHIRE(9142, 9189, 9337, 54, 47), 
+	EMERALD(9142, 9190, 9338, 61, 55), 
+	RUBY(9143, 9191, 9339, 63, 63), 
+	DIAMOND(9143, 9192, 9340, 65, 70), 
+	DRAGON(9144, 9193, 9341, 71, 82), 
+	ONYX(9144, 9194, 9342, 73, 94),
+	AMETHYST(11875, 21338, 21316, 76, 110);
 
 	private final int unfinished, tip, bolt, level, experience;
 
