eb8f1f4bfa0304f96c93f375c289f638d9c400be
Jeff <jeffschabert@gmail.com>
2018-05-19 20:08:02 -0400

    pvm caskets updated  and named

--- src/ethos/model/content/PvmCasket.java
+++ src/ethos/model/content/PvmCasket.java
@@ -35,121 +35,106 @@ public class PvmCasket extends CycleEvent {
 	static {
 		items.put(Rarity.COMMON, 
 			Arrays.asList(
-					new GameItem(386, 250),  
-					new GameItem(995, 10000), 
-					new GameItem(987, 5),
-					new GameItem(560, 2500),
-					new GameItem(565, 2500),
-					new GameItem(562, 3000),
-					new GameItem(566, 2500),
-					new GameItem(985, 5),
-					new GameItem(6128),
-					new GameItem(405, 3),
-					new GameItem(13226),
-					new GameItem(11230, 500),
-					new GameItem(12934, 2500),
-					new GameItem(6129),
-					new GameItem(19484, 250),
-					new GameItem(9243, 500),
-					new GameItem(9244, 300),
-					new GameItem(6130),
-					new GameItem(537, 75), 
-					new GameItem(1704, 10), 
-					new GameItem(1215, 5), 
-					new GameItem(3145, 300),
-					new GameItem(2441, 50),
-					new GameItem(2437, 50),
-					new GameItem(6686, 50),
-					new GameItem(20791, 2),
-					new GameItem(20703, 2),
-					new GameItem(6528),
-					new GameItem(13307, 500),
-					new GameItem(1377),
-					new GameItem(3204),
-					new GameItem(7158),
-					new GameItem(1187),
-					new GameItem(6524),
-					new GameItem(3025, 50),  
-					new GameItem(995, 500000))
+					new GameItem(386, 250),// cooked sharks 
+					new GameItem(995, 10000),// gp 
+					new GameItem(987, 5),// loop half
+					new GameItem(560, 2500),// death rune
+					new GameItem(565, 2500),// blood rune
+					new GameItem(562, 3000),//  chaos rune
+					new GameItem(566, 2500),// soul rune
+					new GameItem(985, 5),// tooth half
+					new GameItem(6128),// rockshell helm
+					new GameItem(405, 3),// pvm casket
+					new GameItem(13226),// herbsack
+					new GameItem(11230, 500),// dragon darts
+					new GameItem(12934, 2500),// zulrah scales
+					new GameItem(6129),// rockshell plate
+					new GameItem(19484, 250),// dragon javelin
+					new GameItem(9243, 500),// diamond b olts e
+					new GameItem(9244, 300),// dragon bolts e
+					new GameItem(6130),// rockshell platelegs
+					new GameItem(537, 75),// dragon bones
+					new GameItem(1705, 10),// amjulet of glory
+					new GameItem(1215, 5),// dragon dagger
+					new GameItem(3145, 300),// cooked karambwan
+					new GameItem(2441, 50),// super strength
+					new GameItem(2437, 50),// super attack
+					new GameItem(6686, 50),// saradomin brews
+					new GameItem(20791, 2),// skill crate
+					new GameItem(20703, 2),// gear box
+					new GameItem(6528),// obsidian maul
+					new GameItem(13307, 500),// blood money
+					new GameItem(1377),// dragon battleaxe
+					new GameItem(3204),// dragon hally
+					new GameItem(7158),// dragon 2h
+					new GameItem(1187),// dragon square shield
+					new GameItem(6524),// obby shield
+					new GameItem(3025, 50),  // super restore(4)
+					new GameItem(995, 500000))// gp
 					
 			);
 			
 		items.put(Rarity.UNCOMMON,
 				Arrays.asList(
-						new GameItem(861),
-						new GameItem(537, 250),
-						new GameItem(6568, 3),
-						new GameItem(13116),
-						new GameItem(4212),
-						new GameItem(4724),
-						new GameItem(4726),
-						new GameItem(4728),
-						new GameItem(4730),
-						new GameItem(4736),
-						new GameItem(4738),
-						new GameItem(11840),
-						new GameItem(11663),
-						new GameItem(11664),
-						new GameItem(11665),
-						new GameItem(8839),
-						new GameItem(405, 5),
-						new GameItem(8840),
-						new GameItem(6737),
-						new GameItem(6733),
-						new GameItem(6731),
-						new GameItem(19675),
-						new GameItem(12696, 100),
-						new GameItem(6686, 200),
-						new GameItem(3025, 200),
-						new GameItem(13307, 750),
-						new GameItem(6536, 3),
-						new GameItem(6524, 3),
-						new GameItem(6525, 3),
-						new GameItem(6526, 3),
-						new GameItem(6522, 400),
-						new GameItem(6527, 3),			
-						new GameItem(392, 500), 
-						new GameItem(1409),
-						new GameItem(995, 1000000))
+						new GameItem(861),// magic short bow
+						new GameItem(537, 250),// dragon bones
+						new GameItem(6568, 3),// obsidian cape
+						new GameItem(4212),// new crystal bow
+						new GameItem(4724),// guthans helm
+						new GameItem(4726),// guthans spear
+						new GameItem(4728),// guthans chain body
+						new GameItem(4730),// guthans chain skirt
+						new GameItem(4736),// karls leather top
+						new GameItem(4738),// karls crossbow
+						new GameItem(11840),// dragon boots
+						new GameItem(405, 5),// pvm casket
+						new GameItem(19675),// arc light
+						new GameItem(12696, 100),// super combat potion
+						new GameItem(6686, 200),// saradomin brew
+						new GameItem(3025, 200),// super restore (4)
+						new GameItem(13307, 750),// blood moeny
+						new GameItem(6522, 400),// obby rings
+						new GameItem(6536),// obby shield		
+						new GameItem(392, 500),// manta ray
+						new GameItem(1409),// ibans staff
+						new GameItem(995, 1000000))// gp
 						
 		);
 			
 			items.put(Rarity.RARE,
 					Arrays.asList(
-							new GameItem(13307, 1500),
-							new GameItem(990, 20),
-							new GameItem(11128),
-							new GameItem(11286),
-							new GameItem(13442, 1000),
-							new GameItem(11937, 1000),
-							new GameItem(11889),
-							new GameItem(2513),
-							new GameItem(2577),
-							new GameItem(10551),
-							new GameItem(10548),
-							new GameItem(6914),
-							new GameItem(19529),
-							new GameItem(6889),
-							new GameItem(13271),
-							new GameItem(13263),
-							new GameItem(12831),
-							new GameItem(12904),
-							new GameItem(11907),
-							new GameItem(12851),
-							new GameItem(12004),
-							new GameItem(19481),
-							new GameItem(13072),
-							new GameItem(11798),
-							new GameItem(13073),
-							new GameItem(12785),
-							new GameItem(11944, 200), 
-							new GameItem(11840),
-							new GameItem(11235),
-							new GameItem(6585),
-							new GameItem(4151, 1),
-							new GameItem(995, 4000000),
-							new GameItem(6199, 5),
-							new GameItem(11818, 1)));
+							new GameItem(13307, 1500),// blood money
+							new GameItem(990, 20),// ckeys
+							new GameItem(11128),// berserker neclass
+							new GameItem(11286),// dragonic vissage
+							new GameItem(13442, 1000),// angler fish
+							new GameItem(11937, 1000),// dark crabs
+							new GameItem(11889),// z hasta
+							new GameItem(2513),// d chainbody
+							new GameItem(2577),// ranger boots
+							new GameItem(10551),// fighter torso
+							new GameItem(10548),// fighter hat
+							new GameItem(6914),// master wand
+							new GameItem(19529),// zenyte shard
+							new GameItem(6889),// mages book
+							new GameItem(13271),// abby dagger
+							new GameItem(13263),// abby bludgeon
+							new GameItem(12831),// blessed spirit shield
+							new GameItem(12904),// toxic staff of the dead
+							new GameItem(11907),// trident of the seas
+							new GameItem(12851),// amulet of the dead
+							new GameItem(12004),// kraken tentacle
+							new GameItem(19481),// heavy balista
+							new GameItem(11798),// godsword blade
+							new GameItem(12785),// ring of wealth i
+							new GameItem(11944, 200), // lava dragon bones
+							new GameItem(11840),// d boots
+							new GameItem(11235),// d bow
+							new GameItem(6585),// fury
+							new GameItem(4151, 1),// whip
+							new GameItem(995, 4000000),// gp
+							new GameItem(6199, 5),// mbox
+							new GameItem(11818, 1)));// gs shard 1
 							
 							
 	}
