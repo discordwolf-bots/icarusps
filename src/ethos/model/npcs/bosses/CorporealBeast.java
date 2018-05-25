51a4f5f0bfdaf20b26f513711d625a449fe9fed9
IcarusVPS <matthewdegnan93@gmail.com>
2018-05-25 22:03:14 +0100

    Corp fix

--- src/ethos/model/npcs/bosses/CorporealBeast.java
+++ src/ethos/model/npcs/bosses/CorporealBeast.java
@@ -63,11 +63,11 @@ public class CorporealBeast {
 	 */
 	public static void attack(Player attacker, Damage damage) {
 		NPC corp = NPCHandler.getNpc(319);
-		int change = Misc.random((int) (damage.getAmount() / 2));
+		//int change = Misc.random((int) (damage.getAmount() / 1));
 		
 		if (corp.getHealth().getAmount() > 50)
-		if (!attacker.getItems().isWearingItem(11824)) {
-			damage.setAmount(change);
+		if (!attacker.getItems().isWearingItem(11824,11889)) {
+			//damage.setAmount(change);
 		}
 	}
 }
