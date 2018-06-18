662484a2ec73fb713e4fee9fc7993a6fba7a55f5
Matthew Degnan <matthewdegnan93@gmail.com>
2018-06-19 00:55:05 +0100

    Update feed fixed

--- src/ethos/discord/main/BotMain.java
+++ src/ethos/discord/main/BotMain.java
@@ -70,11 +70,9 @@ public class BotMain {
 				builder.appendField(emoji + " Pet!", c.getName() + " just got a " + extra[0] + " pet", false);
 				break;
 			case 5: // Update!
-				emoji = bot.getGuildByID(418458996941258752L).getEmojiByName("Pet");
+				emoji = bot.getGuildByID(418458996941258752L).getEmojiByName("Clock");
 				builder.appendField(emoji + " Update!", c.getName() + " has set an update for " + extra[0] + " seconds", false);
-				break;
-				
-				
+				break;				
 		}
 		
 		feedChannel.sendMessage(builder.build());
